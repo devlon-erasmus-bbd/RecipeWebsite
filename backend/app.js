5c16e1dd5e8298807f72b4cddf357aeead5f10ff
devlon-erasmus-bbd <devlon@bbd.co.za>
2023-05-09 21:00:31 +0200

    added main view and stylesheet. updated app to display the view

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const { login, newUser, getUserId } = require('./src/controllers/login');
 const { recipeList, recipeIngredients } = require('./src/controllers/recipes');
@@ -8,6 +9,12 @@ const { recipeList, recipeIngredients } = require('./src/controllers/recipes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
+});
 
 app.get('/login', login);
 app.get('/user', getUserId);
@@ -17,3 +24,4 @@ app.get('/recipes/ingredients', recipeIngredients);
 app.post('/user', newUser);
 
 app.listen(PORT);
+console.log('Running at Port 8080');
