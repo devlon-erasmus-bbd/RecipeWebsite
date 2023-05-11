7b078d731b0ad5dc651e73df2c6e84cf62218e8f
kgotleleloBBD <kgotlelelo@bbd.co.za>
2023-05-11 09:57:19 +0200

    code standard clean up

--- backend/src/controllers/recipes.js
+++ backend/src/controllers/recipes.js
@@ -2,54 +2,57 @@
 const { getRecipeList, getUserRecipes, getRecipeIngredients, postRecipe } = require('../service/index');
 
 async function recipeList(req, res) {
-    try {
-        const data = await getRecipeList();
-        res.json(data[0]);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-    res.end();
+  try {
+    const data = await getRecipeList();
+
+    res.json(data[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+  res.end();
 }
 
 async function userRecipes(req, res) {
-    try {
-        const data = await getUserRecipes(req.query.username);
-        res.json(data[0]);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-    res.end();
+  try {
+    const data = await getUserRecipes(req.query.username);
+
+    res.json(data[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+  res.end();
 }
 
 async function recipeIngredients(req, res) {
-    try {
-        const data = await getRecipeIngredients(req.query.recipeName);
-        res.json(data[0]);
-    } catch (err) {
-        console.log(err);
-        res.status(500);
-    }
-    res.end();
+  try {
+    const data = await getRecipeIngredients(req.query.recipeName);
+
+    res.json(data[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+  res.end();
 
 }
 
 async function createRecipe(req, res) {
-    try {
-        await postRecipe(req.body);
-        res.status(201);
-    } catch (err) {
-        console.log(err);
-        res.status(409);
-    }
-    res.end();
+  try {
+    await postRecipe(req.body);
+    res.status(201);
+  } catch (err) {
+    console.log(err);
+    res.status(409);
+  }
+  res.end();
 
 }
 
 module.exports = {
-    recipeList,
-    recipeIngredients,
-    createRecipe,
-    userRecipes
+  recipeList,
+  recipeIngredients,
+  createRecipe,
+  userRecipes
 };
