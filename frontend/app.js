a7d826ac2b6c9a40fa2d7c9ceafbe8ccef99aa05
G994618 <devlone@sanlaminvestments.com>
2023-05-15 16:17:20 +0200

    Moved views

---
+++ frontend/app.js
@@ -0,0 +1,53 @@
+/* global process, __dirname */
+
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+const {
+  login,
+  signup,
+  getUser,
+  userDeatils,
+  recipeList,
+  recipeIngredients,
+  userRecipes,
+  createRecipe,
+  searchMealDb,
+  categoryList,
+  recipeByCategory
+} = require('./src/controllers/index');
+
+const { auth } = require('./src/middleware/index');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+// app.use(auth);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
+});
+
+app.post('/login', login);
+app.post('/signup', signup);
+app.get('/user', getUser);
+app.get('/user-details/:email', userDeatils);
+
+app.get('/recipes/user', userRecipes);
+app.get('/recipes/list', recipeList);
+app.get('/recipes/ingredients', recipeIngredients);
+app.get('/recipes/search', searchMealDb);
+app.get('/recipes/category', recipeByCategory);
+app.post('/recipes', createRecipe);
+
+app.get('/categories/list', categoryList);
+
+app.listen(PORT);
+console.log(`Running at Port ${PORT}`);
