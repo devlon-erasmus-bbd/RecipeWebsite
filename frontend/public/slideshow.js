26ffa5d89016a5e45dabbc2fb0937749f8c386f8
george_pauer <george.pauer1@vcontractor.co.za>
2023-05-17 12:29:34 +0200

    changes

--- frontend/public/slideshow.js
+++ frontend/public/slideshow.js
@@ -4,7 +4,7 @@ let images = ['food1.jpg', 'food2.jpg', 'food3.jpg', 'food4.jpg', 'food5.jpg'];
 let index = 0;
 
 const changeImage = () => {
-  slideshowContainer.style.backgroundImage = `url('/slideshow/${images[index]}')`;
+  slideshowContainer.style.backgroundImage = `url('../public/slideshow/${images[index]}')`;
   index = (index + 1) % images.length;
   setTimeout(changeImage, 5000); // Change image every 5 seconds
 }
